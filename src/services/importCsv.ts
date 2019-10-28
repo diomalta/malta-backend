999b6ff75469d27805d4ac90284f7782f81e0d08
Diego Santos <diego.santos@cubos.io.com>
2019-10-27 23:36:57 -0300

    fix tsc

--- src/services/importCsv.ts
+++ src/services/importCsv.ts
@@ -19,58 +19,58 @@ export default class ImportService {
     try {
       const years = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2019'];
 
-      for (const year of years) {
-        const results = [];
-        await this.getDataFile(path.join(__dirname, `../resources/csv/planilha-maltachurrascos-${year}.csv`), results);
+      // for (const year of years) {
+      //   const results = [];
+      //   await this.getDataFile(path.join(__dirname, `../resources/csv/planilha-maltachurrascos-${year}.csv`), results);
 
-        for (const row of results) {
-          if (!this.isBlank(row.client)) {
-            const email = !this.isBlank(row.email)
-              ? row.email
-              : `${row.client
-                  .trim()
-                  .replace(' ', '')
-                  .toLowerCase()}-${row.contact.trim().replace(' ', '')}@notEmail.com`;
+      //   for (const row of results) {
+      //     if (!this.isBlank(row.client)) {
+      //       const email = !this.isBlank(row.email)
+      //         ? row.email
+      //         : `${row.client
+      //             .trim()
+      //             .replace(' ', '')
+      //             .toLowerCase()}-${row.contact.trim().replace(' ', '')}@notEmail.com`;
 
-            let client = await this.clientModel.findOne({ email });
-            if (!client) {
-              const [telefone, celular, contato] = !this.isBlank(row.contacts)
-                ? (row.contacts.split('/').map((s: string) => this.removeBlank(s)) as string[])
-                : [];
-              client = (await clientServiceInstance.Register({
-                name: this.captalize(
-                  row.client
-                    .trim()
-                    .replace(' ', '')
-                    .toLowerCase(),
-                ),
-                email,
-                telefone,
-                celular,
-                contato,
-              } as IClientInputDTO)).client;
-            }
+      //       let client = await this.clientModel.findOne({ email });
+      //       if (!client) {
+      //         const [telefone, celular, contato] = !this.isBlank(row.contacts)
+      //           ? (row.contacts.split('/').map((s: string) => this.removeBlank(s)) as string[])
+      //           : [];
+      //         client = (await clientServiceInstance.Register({
+      //           name: this.captalize(
+      //             row.client
+      //               .trim()
+      //               .replace(' ', '')
+      //               .toLowerCase(),
+      //           ),
+      //           email,
+      //           telefone,
+      //           celular,
+      //           contato,
+      //         } as IClientInputDTO)).client;
+      //       }
 
-            this.logger.silly('Creating event db record');
-            const eventRecord = await this.eventModel.create({
-              client: client._id,
-              data: !this.isBlank(row.date) ? new Date('20' + this.formatDate(row.date)) : '',
-              convidados: !this.isBlank(row.count) ? parseInt(row.count.trim(), 10) : 0,
-              valorUnitario: !this.isBlank(row.value) ? parseFloat(row.value.replace('R$', '').trim()) : 0,
-              tipoEvento: !this.isBlank(row.type) ? row.type.trim().toLowerCase() : 'Nao informado',
-              endereco: !this.isBlank(row.adress) ? row.adress.trim() : 'Nao informado',
-              status: 'Confirmado',
-            });
+      //       this.logger.silly('Creating event db record');
+      //       const eventRecord = await this.eventModel.create({
+      //         client: client._id,
+      //         data: !this.isBlank(row.date) ? new Date('20' + this.formatDate(row.date)) : '',
+      //         convidados: !this.isBlank(row.count) ? parseInt(row.count.trim(), 10) : 0,
+      //         valorUnitario: !this.isBlank(row.value) ? parseFloat(row.value.replace('R$', '').trim()) : 0,
+      //         tipoEvento: !this.isBlank(row.type) ? row.type.trim().toLowerCase() : 'Nao informado',
+      //         endereco: !this.isBlank(row.adress) ? row.adress.trim() : 'Nao informado',
+      //         status: 'Confirmado',
+      //       });
 
-            if (!eventRecord) {
-              this.logger.silly(`Row failed insert event\n: ${row}`);
-            }
+      //       if (!eventRecord) {
+      //         this.logger.silly(`Row failed insert event\n: ${row}`);
+      //       }
 
-            client = await this.clientModel.findOne({ email });
-            await client.update({ quantidadeEventos: client.quantidadeEventos + 1 });
-          }
-        }
-      }
+      //       client = await this.clientModel.findOne({ email });
+      //       await client.update({ quantidadeEventos: client.quantidadeEventos + 1 });
+      //     }
+      //   }
+      // }
 
       console.warn(`finished`);
 
